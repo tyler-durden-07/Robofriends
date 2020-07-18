56146e049edd2d6b2e76d7664ceaa2f656ea37cf
tyler-durden-07 <saurabhsinghtiari2811@gmail.com>
2020-07-18 09:37:43 +0530

    This is the Robofriends app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import CardList from './CardList.js';
+import SearchBox from './SearchBox.js';
 import './App.css';
+import Scroll from './Scroll.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    constructor(){
+        super()
+        this.state={
+            robots:[],
+            searchField:''
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users').then(response=>{
+           return  response.json();
+        })
+        .then(users=>{
+            this.setState({robots:users})
+        });
+    }
+
+    onSearchChange = (event) => {
+       this.setState({searchField:event.target.value})
+       
+    }
+
+    render(){
+        const filteredRobots=this.state.robots.filter(robots=>{
+            return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        })
+        if(this.state.robots.length==0){
+            return <h1>Loading...</h1>
+        }else{
+        return (
+            <div className="tc">
+                <h1 className="f1">Robofriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
+            </div>
+        );
+    }
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.js';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +16,5 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
